13cd6e5c3da0edbb105747dc4b32b227bbfe3722
AndreBelo96 <andreablt96@hotmail.it>
2020-07-15 23:24:55 +0200

    Commit Baby

--- core/src/com/mygdx/game/LoginScreen.java
+++ core/src/com/mygdx/game/LoginScreen.java
@@ -1,6 +1,7 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
@@ -11,7 +12,9 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 
 
 import java.util.ArrayList;
@@ -19,10 +22,13 @@ import java.util.ArrayList;
 public class LoginScreen extends ScreenAdapter {
 
     private SpriteBatch batch;
+    private Skin skin;
     private OrthographicCamera camera;
     private TextButton reloadbutton,exitbutton,nextbutton;
     private Stage stage;
     private final Label name_label, pass_label;
+    private final TextField nameText, passText;
+    private final Preferences prefs;
     private BitmapFont font;
     private Actor actor;
 
@@ -34,32 +40,35 @@ public class LoginScreen extends ScreenAdapter {
         //camera.setToOrtho(false,Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
         TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
         font = new BitmapFont(Gdx.files.internal("roboto_light.fnt"));
+        skin = new Skin(Gdx.files.internal("skin.json"));
         font.setColor(Color.WHITE);
         style.font = font;
+        prefs = Gdx.app.getPreferences("Zawardo");
         Label.LabelStyle label_style = new Label.LabelStyle(font,Color.WHITE);
         name_label = new Label("Name: ", label_style);
         pass_label = new Label("Password: ", label_style);
-        /*TextButton Name_Label = new TextButton(Constant.WIN_TEXT,style);
-        TextButton Password_Label = new TextButton(Constant.);*/
-        reloadbutton=new TextButton(Constant.RELOAD_TEXT,style);
-        nextbutton=new TextButton(Constant.NEXT_TEXT,style);
-        exitbutton=new TextButton(Constant.EXIT_TEXT,style);
+        nameText = new TextField(prefs.getString("name", ""), skin);
+        passText = new TextField("", skin);
+        reloadbutton = new TextButton(Constant.RELOAD_TEXT,style);
+        nextbutton = new TextButton(Constant.NEXT_TEXT,style);
+        exitbutton = new TextButton(Constant.EXIT_TEXT,style);
 
         name_label.setBounds(Gdx.graphics.getWidth()/2-300,Gdx.graphics.getHeight()-200,200,100);
-        pass_label.setBounds(Gdx.graphics.getWidth()/2-300,Gdx.graphics.getHeight()-240,200,100);
+        pass_label.setBounds(Gdx.graphics.getWidth()/2-300,Gdx.graphics.getHeight()-250,200,100);
+        nameText.setBounds(Gdx.graphics.getWidth()/2-100,Gdx.graphics.getHeight()-170,200,50);
+        passText.setBounds(Gdx.graphics.getWidth()/2-100,Gdx.graphics.getHeight()-220,200,50);
         reloadbutton.setBounds(Gdx.graphics.getWidth()/2-100,Gdx.graphics.getHeight()/2-50,200,100);
         nextbutton.setBounds(Gdx.graphics.getWidth()/2-100,Gdx.graphics.getHeight()/2-150,200,100);
         exitbutton.setBounds(Gdx.graphics.getWidth()/2-100,Gdx.graphics.getHeight()/2-250,200,100);
 
-        /*reloadbutton.addListener(listener.get(0));
-        nextbutton.addListener(listener.get(1));
-        exitbutton.addListener(listener.get(2));*/
 
         stage=new Stage();
         stage.clear();
         Gdx.input.setInputProcessor(stage);
         stage.addActor(name_label);
         stage.addActor(pass_label);
+        stage.addActor(nameText);
+        stage.addActor(passText);
         stage.addActor(reloadbutton);
         stage.addActor(nextbutton);
         stage.addActor(exitbutton);
@@ -69,10 +78,12 @@ public class LoginScreen extends ScreenAdapter {
     public void render(float delta) {
         Gdx.gl.glClearColor(0,0,0,0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        stage.act();
+
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
-        stage.draw();
         batch.end();
+
+        stage.draw();
+        stage.act();
     }
 }
